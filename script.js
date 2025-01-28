b82104b5c6a53e35706de0f9274c7b3dd4105c37
Кирилл Каптелий <161931268+KirillKapteily@users.noreply.github.com>
2025-01-28 21:48:04 +0200

    Add modal, tried to fix bugs
    Version 1.2
    Add modal.
    Tried to fix backgrounds.

---
+++ script.js
@@ -0,0 +1,17 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      closeModalBtn2: document.querySelector("[data-modal-close-2]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn2.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
